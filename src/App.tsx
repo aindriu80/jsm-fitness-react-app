7ce54f05eb2e721a4aeaae37d133fc12d8016d00
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-04-13 15:29:23 +0100

    Header, Footer, Navbar initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Dia Dhuit an Domhain</h1>
-    </div>
-  );
-}
+const App = () => (
+      <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+      </Box>
+);
 
 export default App;
