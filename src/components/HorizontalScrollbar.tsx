cb7ce772f0b9a792122f48bf48b9dbcd7e1b96c5
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-04-17 11:40:41 +0100

    Creating body part section - initial

--- src/components/HorizontalScrollbar.tsx
+++ src/components/HorizontalScrollbar.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Box } from "@mui/material";
+import BodyPart from "./BodyPart";
 
-const HorizontalScrollbar = () => {
-  return <div>HorizontalScrollbar</div>;
+interface Item {
+  id: string | number;
+  title: string;
+}
+
+interface HorizontalScrollbarProps {
+  data: Item[];
+  bodyPart: object;
+  setBodyPart: object;
+}
+
+const HorizontalScrollbar: React.FC<HorizontalScrollbarProps> = ({
+  data,
+  bodyPart,
+  setBodyPart,
+}) => {
+  return (
+    <div>
+      {data.map((item: Item) => (
+        <Box
+          key={item.id.toString() || item}
+          itemId={item.id || item}
+          title={item.title || item}
+          m="0 40px"
+        >
+          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+        </Box>
+      ))}
+    </div>
+  );
 };
 
 export default HorizontalScrollbar;
--- src/components/SearchExercises.tsx
+++ src/components/SearchExercises.tsx
@@ -3,19 +3,29 @@ import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+interface SearchExercisesProps {
+  setExercises: object;
+  bodyPart: object;
+  setBodyPart: object;
+}
+
+const SearchExercises: React.FC<SearchExercisesProps> = ({
+  setExercises,
+  bodyPart,
+  setBodyPart,
+}) => {
   const [search, setSearch] = useState("");
-  const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
     const fetchExercisesData = async () => {
       const bodyPartsData = await fetchData(
         "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
-        exercises,
+        exerciseOptions,
       );
 
       setBodyParts(["all", ...bodyPartsData]);
+      fetchExercisesData();
     };
   }, []);
 
@@ -79,7 +89,11 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: "relative", width: "10%", p: "20px" }}>
-        <HorizontalScrollbar data={bodyParts} />
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
   );
