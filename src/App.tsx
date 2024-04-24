9660c0e4a2d181f3f83f7357254e0dcab5841406
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-04-24 11:05:15 +0100

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { Route, Routes } from "react-router-dom";
-import { Box } from "@mui/material";
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-import ExerciseDetail from "./pages/ExerciseDetail";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-import "./App.css";
+import './App.css';
 
 const App = () => (
-      <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/exercise/:id" element={<ExerciseDetail />} />
-        </Routes>
-        <Footer />
-      </Box>
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
 );
 
 export default App;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Box, Stack, Typography } from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
+        <Typography variant="h5" pb="40px" mt="20px">
+          Cruthaithe Aibreán 2024 A.M.G.E.
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
